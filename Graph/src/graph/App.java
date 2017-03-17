a009add83fe97d9553d76fe9262f034dbfea5c8a
Skjelsbek <skjelsbek@hotmail.com>
2017-03-17 10:16:24 +0100

    Added Graph project

---
+++ Graph/src/graph/App.java
@@ -0,0 +1,17 @@
+package graph;
+
+public class App {
+    public static void main(String[] args) {
+        Hashmap<Integer, String> hm = new Hashmap(8);
+        
+        String[] array = {"Jon", "Martin", "Nils", "Marius", "Vegard", "Vegar", "Espen", "Lars", "Olav", "Jens"};
+        for (int i = 0; i < 10; i++) {
+            hm.insert(i, array[i]);
+        }
+        System.out.println(hm);
+        hm.delete(0);        
+        System.out.println(hm);
+        hm.delete(9);        
+        System.out.println(hm);
+    }
+}
---
+++ Graph/src/graph/Hashmap.java
@@ -0,0 +1,60 @@
+package graph;
+
+public class Hashmap<TKey, TValue> {
+
+    Node<TKey, TValue>[] array;
+
+    public Hashmap(int size) {
+        array = new Node[size];
+    }
+
+    public void insert(TKey key, TValue data) {
+        int arrayIndex = getIndex(key);
+        if (array[arrayIndex] == null) {
+            array[arrayIndex] = new Node(key, data);
+        } else {
+            array[arrayIndex].insert(key, data);
+        }
+    }
+
+    public TValue get(TKey key) {
+        int arrayIndex = getIndex(key);
+        if (array[arrayIndex] != null) {
+            return array[arrayIndex].get(key).data;
+        }
+        return null;
+    }
+
+    private int getIndex(TKey key) {
+        return (Math.abs(key.hashCode()) % array.length);
+    }
+
+    public void delete(TKey key) {
+        int arrayIndex = getIndex(key);
+        if (array[arrayIndex] != null) {
+            if (array[arrayIndex].key == key) {
+                if (array[arrayIndex].nextNode == null) {
+                    array[arrayIndex] = null;
+                } else {
+                    array[arrayIndex] = array[arrayIndex].nextNode;
+                }
+            } else if (array[arrayIndex].nextNode != null) {
+                array[arrayIndex].delete(key);
+            } else {
+                System.out.println("There's no such key to be found!");
+            }
+        }
+    }
+    
+    @Override
+    public String toString() {
+        String s = "Hashmap:\n";
+        for(int i=0; i<array.length; i++){
+            if (array[i] != null){
+                s += i + ": " + array[i].toString();
+            }
+            s += (s.charAt(s.length()-1) == '\n')?"":"\n";
+        }
+        return s;
+    }
+}
